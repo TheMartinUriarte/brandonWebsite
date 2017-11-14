efba062a8d710a095b9204337d1690537606f489
Martin Uriarte <themartinuriarte.programmer@gmail.com>
2017-11-13 16:59:15 -0800

    Brandon Frere setup - more setup work required. Adding featured images next

---
+++ wp-content/themes/uriartetwentyseventeen/footer.php
@@ -0,0 +1,12 @@
+</div><!-- /.container -->
+
+<footer class="blog-footer">
+  <p>Early build website for Brandon Frere by <a href="https://github.com/TheMartinUriarte">Martin Uriarte</a>.</p>
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/uriartetwentyseventeen/functions.php
@@ -0,0 +1,65 @@
+<?php
+
+// Adds scripts and stylesheets
+  function brandonWebsite_scripts() {
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
+    wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.css', array( 'jquery' ), '3.3.6', true );
+  }
+
+  add_action( 'wp_enqueue_scripts', 'brandonWebsite_scripts' );
+
+// Adds custom Google fonts
+  function brandonWebsite_google_fonts() {
+    wp_register_style( 'Raleway | Roboto', 'href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet"');
+    wp_enqueue_style( 'Raleway | Roboto' );
+  }
+
+  add_action ('wp_print_styles', 'brandonWebsite_google_fonts');
+
+// WordPress Titles
+  add_theme_support( 'title-tag' );
+
+// Custom Settings Dashboard Option
+  function custom_settings_add_menu() {
+    add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99 );
+  }
+
+  add_action( 'admin_menu', 'custom_settings_add_menu' );
+
+// Create Custom Global Settings
+  function custom_settings_page() { ?>
+    <div class="wrap">
+      <h1>Custom Settings</h1>
+      <form method="post" action="options.php">
+        <?php
+          settings_fields( 'section' );
+          do_settings_sections( 'theme-options' );
+          submit_button();
+        ?>
+      </form>
+    </div>
+  <?php }
+
+// LinkedIn
+  function setting_linkedin() { ?>
+    <input type="text" name="linkedin" id="linkedin" value="<?php echo get_option( 'linkedin' ); ?>" />
+  <?php }
+
+// Twitter
+  function setting_twitter() { ?>
+    <input type="text" name="twitter" id="twitter" value="<?php echo get_option( 'twitter' ); ?>" />
+  <?php }
+
+// Custom Settings Page Options
+  function custom_settings_page_setup() {
+    add_settings_section( 'section', 'All Settings', null, 'theme-options' );
+
+    add_settings_field( 'linkedin', 'LinkedIn URL', 'setting_linkedin', 'theme-options', 'section' );
+    add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section' );
+
+    register_setting('section', 'linkedin');
+    register_setting('section', 'twitter');
+  }
+
+  add_action( 'admin_init', 'custom_settings_page_setup' );
