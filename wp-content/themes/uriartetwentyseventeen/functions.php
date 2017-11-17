06439fa18c0dae269f8d965f50ab700eb6238984
Martin Uriarte <themartinuriarte.programmer@gmail.com>
2017-11-17 11:07:45 -0800

    [WIP] Corrections to Bootstrap JS

--- wp-content/themes/uriartetwentyseventeen/functions.php
+++ wp-content/themes/uriartetwentyseventeen/functions.php
@@ -4,7 +4,7 @@
   function brandonWebsite_scripts() {
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
     wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.css', array( 'jquery' ), '3.3.6', true );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
   }
 
   add_action( 'wp_enqueue_scripts', 'brandonWebsite_scripts' );
--- wp-content/themes/uriartetwentyseventeen/header.php
+++ wp-content/themes/uriartetwentyseventeen/header.php
@@ -55,7 +55,7 @@
           </nav>
         </div>
       </div> -->
-      <nav class="navbar navbar-default">
+      <nav class="navbar navbar-white">
         <div class="container-fluid">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
