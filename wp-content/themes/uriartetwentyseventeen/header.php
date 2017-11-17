dc08580394a28749580713c748ce99e1b172273c
Martin Uriarte <themartinuriarte.programmer@gmail.com>
2017-11-16 17:00:06 -0800

    [WIP] Change-up on nav style

--- wp-content/themes/uriartetwentyseventeen/header.php
+++ wp-content/themes/uriartetwentyseventeen/header.php
@@ -19,11 +19,10 @@
 
     <body>
 
-      <div class="bf-trim"></div>
+      <!-- <div class="bf-trim"></div>
       <div class="blog-masthead">
         <div class="container">
           <nav class="blog-nav">
-            <!-- <a class="blog-nav-item active" href="#">Home</a> -->
             <div class="bf-nav-left" role="navigation">
               <nav id="bf-menu-primary">
                 <ul id="bf-nav-menu-left" class="bf-nav-menu">
@@ -38,7 +37,7 @@
             </div>
             <div class="bf-wrap-logo">
               <a class="bf-site-logo" href="#">
-                <img src="../wp-content/uploads/2017/11/bfLogoDraft.png" alt="Brandon Frere Logo">
+                <img src="/brandonWebsite/wp-content/uploads/2017/11/bfLogoDraft.png" alt="Brandon Frere Logo">
               </a>
             </div>
             <div class="bf-nav-right" role="navigation">
@@ -55,6 +54,62 @@
             </div>
           </nav>
         </div>
-      </div>
+      </div> -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a href="#">
+              <img class="bf-site-logo" src="/brandonWebsite/wp-content/uploads/2017/11/bfLogoDraft.png" alt="Brandon Frere Logo">
+            </a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Link</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Link</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+
 
       <div class="container">
