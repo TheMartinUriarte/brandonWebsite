40c8201da1612cb0c5d640a84b76594c25747df9
Martin Uriarte <themartinuriarte.programmer@gmail.com>
2017-11-17 11:58:03 -0800

    [WIP] Addition of mobile and non-mobile nav logos

--- wp-content/themes/uriartetwentyseventeen/header.php
+++ wp-content/themes/uriartetwentyseventeen/header.php
@@ -19,44 +19,8 @@
 
     <body>
 
-      <!-- <div class="bf-trim"></div>
-      <div class="blog-masthead">
-        <div class="container">
-          <nav class="blog-nav">
-            <div class="bf-nav-left" role="navigation">
-              <nav id="bf-menu-primary">
-                <ul id="bf-nav-menu-left" class="bf-nav-menu">
-                  <li class="blog-nav-item">
-                    <a href="./about/">About<p>Meet Brandon</p></a>
-                  </li>
-                  <li>
-                    <a href="./work/">Work<p>His Passion</p></a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div class="bf-wrap-logo">
-              <a class="bf-site-logo" href="#">
-                <img src="/brandonWebsite/wp-content/uploads/2017/11/bfLogoDraft.png" alt="Brandon Frere Logo">
-              </a>
-            </div>
-            <div class="bf-nav-right" role="navigation">
-              <nav id="bf-menu-secondary">
-                <ul id="bf-nav-menu-right" class="bf-nav-menu">
-                  <li>
-                    <a href="./news/">News<p>The Latest in the Media</p></a>
-                  </li>
-                  <li>
-                    <a href="./contact/">Contact<p>Get in Touch</p></a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </nav>
-        </div>
-      </div> -->
       <nav class="navbar navbar-white">
-        <div class="container-fluid">
+        <div class="container-fluid bf-container">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -65,47 +29,23 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a href="#">
-              <img class="bf-site-logo" src="/brandonWebsite/wp-content/uploads/2017/11/bfLogoDraft.png" alt="Brandon Frere Logo">
+            <a class="bf-mobile-logo" href="#">
+              <img class="bf-logo" src="/brandonWebsite/wp-content/uploads/2017/11/bfLogoDraft.png" alt="Brandon Frere Logo">
             </a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-              <li><a href="#">Link</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+            <ul class="nav navbar-nav navbar-left">
+              <li><a href="./about">About<p>Meet Brandon</p></a></li>
+              <li><a href="./work">Work<p>His Passion</p></a></li>
             </ul>
-            <form class="navbar-form navbar-left">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search">
-              </div>
-              <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+            <a class="bf-site-logo" href="#">
+              <img class="bf-logo" src="/brandonWebsite/wp-content/uploads/2017/11/bfLogoDraft.png" alt="Brandon Frere Logo">
+            </a>
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Link</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
-              </li>
+              <li><a href="./news">News<p>The Latest in the Media</p></a></li>
+              <li><a href="./contact">Contact<p>Get in Touch</p></a></li>
             </ul>
           </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
